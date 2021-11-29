870b058fff6f98a0dbb592d51b6e6e85a910e5e6
Jackburton79 <stefano.ceccherini@gmail.com>
2021-11-29 09:16:43 +0100

    Convert PrefsWindow to Layout system

--- PrefsWindow.cpp
+++ PrefsWindow.cpp
@@ -6,6 +6,7 @@
 
 #include <Box.h>
 #include <Button.h>
+#include <LayoutBuilder.h>
 #include <TabView.h>
 
 #include <cstdio>
@@ -13,21 +14,27 @@
 const BRect kFrame(0, 0, 400, 250);
 
 TPrefsWindow::TPrefsWindow()
-	:BWindow(kFrame, "Options Window", B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS)
+	:BWindow(kFrame, "Options Window", B_TITLED_WINDOW,
+		B_ASYNCHRONOUS_CONTROLS|B_AUTO_UPDATE_SIZE_LIMITS)
 {
-	//center_window_on_screen(this);	
-	
-	BBox *box = new BBox(Bounds(), "Background");
-	box->ResizeBy(1, 1);
+	SetLayout(new BGroupLayout(B_HORIZONTAL));
+
+	BBox *box = new BBox("Background");
+	AddChild(box);
 	
 	fTabView = new BTabView(Bounds().InsetByCopy(10, 10), "Tabs view");
+	BView* layoutView = BLayoutBuilder::Group<>()
+		.SetInsets(B_USE_DEFAULT_SPACING)
+		.Add(fTabView)
+		.View();
 	
-	box->AddChild(fTabView);
+	box->AddChild(layoutView);
 	
 	BRect contentRect(fTabView->ContainerView()->Bounds());	
 	fTabView->AddTab(new TGeneralOptionsView(contentRect));
 	fTabView->AddTab(new TFileLocationsView(contentRect));	
-	AddChild(box);
+
+	CenterOnScreen();
 }
 
 
